26209a68752a48d34c6713d8837823b9442b53c3
LeonardoDiCaterina <20240485@novaims.unl.pt>
2025-05-07 17:33:05 +0100

    random_split function

--- utils/prep_data.py
+++ utils/prep_data.py
@@ -1,5 +1,6 @@
 import torch
-
+from sklearn.model_selection import train_test_split
+import pandas as pd
 def feature_engineering(data, manual_row_removal=False):
     y = data['CRUDE PROTEIN'] 
     X = data.drop(columns=['CRUDE PROTEIN', 'WING TAG', 'EMPTY MUSCULAR STOMACH'])
@@ -17,3 +18,25 @@ def preprocess_data(X, y):
     X = torch.tensor(X.values, dtype=torch.float32)
     y = torch.tensor(y.values, dtype=torch.float32)
     return X, y
+
+
+def random_split(X, y, val_size=0.2):
+    """
+    Splits the data into training and testing sets, preprocesses the data, and returns the final training and testing sets.
+
+    Args:
+        X (pd.DataFrame): training data
+        y (pd.Series): target variable
+        val_size (float, optional): proportion of the dataset to include in the test split. Defaults to 0.2.
+
+    Returns:
+        tuple: preprocessed training and testing data
+    
+    """
+    
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_size)
+    
+    X_final_train, y_final_train = preprocess_data(X_train, y_train)
+    X_final_val, y_final_val = preprocess_data(X_val, y_val)
+    
+    return X_final_train, X_final_val, y_final_train, y_final_val
